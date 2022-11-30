f3017005c8d0a9b6b8e488418ea2544db41cb388
Taner Solak <56222973+tanersolak@users.noreply.github.com>
2022-11-30 16:13:56 +0300

    Update SETTINGS.py

--- SETTINGS.py
+++ SETTINGS.py
@@ -1,7 +1,7 @@
 import glob
 
 dir_path = 'data/'        # Path for images
-img = f"{dir_path}*.jpg"
+img = f"{dir_path}*.jpg"  # You can change type of your image here
 
 weights = glob.glob("yolo/*.weights")[0]  # Path for weights
 labels = glob.glob("yolo/*.txt")[0]       # Path for labels
